8dfa4f41318a72a23ff86f92c79fbad36cdd1d7d
seriksk1 <seriksk1@ukr.net>
2021-04-01 18:56:54 +0300

    main

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker ...', event)
+    event.waitUntil(
+        caches.open("static")
+            .then(function(cache) {
+                console.log("precaching")
+                cache.add('/Homepage.html')
+                cache.add('/')
+            })
+    )
 })
 
 self.addEventListener('activate', function(event) {
@@ -9,6 +17,15 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
     console.log('[Service Worker] Fetching something ...', event)
-    event.respondWith(fetch(event.request))
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response)
+                    return response
+                else
+                    return fetch(event.request)
+            }
+        )
+    )
 })
 
